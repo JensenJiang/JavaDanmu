694818946a04083bb38ed3f4ecaac606e134b000
Cyano Hao <c@cyano.cn>
2016-05-24 00:38:55 +0800

    add app init window

---
+++ danmaQ/src/danmaQ/App.java
@@ -0,0 +1,137 @@
+package danmaQ;
+
+import com.trolltech.qt.gui.*;
+
+import java.util.List;
+
+
+public class App extends QWidget
+{
+    int lineHeight;
+    int fontSize;
+    int screenCount;
+    String fontFamily;
+    float speedScale;
+
+    QLineEdit server = new QLineEdit("http://danmaku.cc:5000", this);
+    QLineEdit channel = new QLineEdit("demo", this);
+    QLineEdit passwd = new QLineEdit("", this);
+
+    QPushButton configBtn = new QPushButton("&config", this);
+    QPushButton hideBtn = new QPushButton("&Hide", this);
+    QPushButton mainBtn = new QPushButton("&Subscribe", this);
+
+    List<QWidget> dmWindows;
+    Subscriber subscriber;
+    TrayIcon trayIcon;
+
+    public static void main(String []args)
+    {
+        QApplication app = new QApplication(args);
+
+        QDesktopWidget desktop = QApplication.desktop();
+        App dm_app = new App();
+        app.exec();
+    }
+
+    App()
+    {
+        this.setWindowTitle("Danmaku");
+        this.setWindowIcon(new QIcon("res/statusicon.png"));
+        this.trayIcon = new TrayIcon(this);
+
+        QVBoxLayout layout = new QVBoxLayout(this);
+        QHBoxLayout hbox = new QHBoxLayout(layout.widget());
+        hbox.addWidget(new QLabel("Server: ", this));
+        hbox.addWidget(this.server);
+        layout.addLayout(hbox);
+
+        hbox = new QHBoxLayout(layout.widget());
+        hbox.addWidget(new QLabel("Channel: ", this));
+        hbox.addWidget(this.channel);
+        layout.addLayout(hbox);
+
+        hbox = new QHBoxLayout(layout.widget());
+        hbox.addWidget(new QLabel("Password: ", this));
+        this.passwd.setEchoMode(QLineEdit.EchoMode.Password);
+        hbox.addWidget(this.passwd);
+        layout.addLayout(hbox);
+
+        hbox = new QHBoxLayout(layout.widget());
+        this.configBtn.setEnabled(false);
+        hbox.addWidget(this.hideBtn);
+        hbox.addWidget(this.configBtn);
+        hbox.addWidget(this.mainBtn);
+        layout.addLayout(hbox);
+
+        this.setLayout(layout);
+
+        this.fontSize = 36;
+        this.lineHeight = (int)(this.fontSize * 1.2);
+        this.fontFamily =
+            "WenQuanYi Micro Hei, Source Han Sans CN, WenQuanYi Zen Hei," +
+                    "Microsoft YaHei, SimHei, " +
+                    "STHeiti, Hiragino Sans GB, " +
+                    "sans-serif";
+        this.speedScale = 1.0f;
+
+        this.subscriber = null;
+    }
+}
+
+class TrayIcon extends QSystemTrayIcon
+{
+    QAction toggleAction;
+    QAction showAction;
+    QAction aboutAction;
+    QAction exitAction;
+    QAction refreshScreenAction;
+
+    QIcon iconRunning;
+    QIcon iconStopped;
+
+    TrayIcon(QWidget parent)
+    {
+        super(parent);
+
+        this.iconRunning = new QIcon("res/statusicon.png");
+        this.iconStopped = new QIcon("res/statusicon_disabled.png");
+        this.setIcon(this.iconStopped);
+
+        QMenu menu = new QMenu(parent);
+        this.toggleAction = menu.addAction("Toggle Subscription");
+        this.refreshScreenAction = menu.addAction("Refresh Screen");
+        this.showAction = menu.addAction("Show Main Window");
+        this.aboutAction = menu.addAction("About");
+        this.exitAction = menu.addAction("Exit");
+
+        this.setContextMenu(menu);
+
+        this.activated.connect(this, "onActivated(com.trolltech.qt.gui.QSystemTrayIcon$ActivationReason)");
+        this.show();
+    }
+
+    void onActivated(ActivationReason e)
+    {
+        if (e.equals(ActivationReason.Trigger))
+        {
+            QWidget parent = (QWidget) this.parent();
+            if (parent == null)
+                return;
+            if (parent.isVisible())
+                parent.hide();
+            else
+                parent.show();
+        }
+    }
+
+    void setIconRunning()
+    {
+        this.setIcon(this.iconRunning);
+    }
+
+    void setIconStopped()
+    {
+        this.setIcon(this.iconStopped);
+    }
+}

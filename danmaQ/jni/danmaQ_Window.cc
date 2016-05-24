60487e3ede036c5bc19b64f8f9c6ba0364264040
Cyano Hao <c@cyano.cn>
2016-05-24 20:16:21 +0800

    fix mouse event on linux

--- danmaQ/jni/danmaQ_Window.cc
+++ danmaQ/jni/danmaQ_Window.cc
@@ -5,6 +5,8 @@
 #include <X11/Xregion.h>
 #include <X11/Xutil.h>
 #include <X11/extensions/shape.h>
+
+extern "C" Display *_ZN8QX11Info7displayEv();
 #endif
 
 #include <iostream>
@@ -13,7 +15,7 @@ extern "C" JNIEXPORT void JNICALL Java_danmaQ_Window_xHacks(JNIEnv *, jobject,
                                                             jlong winId) {
 #ifdef __linux
   Region region = XCreateRegion();
-  Display *display = XOpenDisplay(":0");
+  Display *display = _ZN8QX11Info7displayEv();
   std::cout << display << std::endl;
   XShapeCombineRegion(display, winId, ShapeInput, 0, 0, region, ShapeSet);
 #endif

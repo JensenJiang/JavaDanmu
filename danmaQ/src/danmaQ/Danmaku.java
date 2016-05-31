69e97999fc1c8d28f6131337a66dcee21d08be59
07akioni <07akioni2@gmail.com>
2016-05-31 19:53:19 +0800

    UI

--- danmaQ/src/danmaQ/Danmaku.java
+++ danmaQ/src/danmaQ/Danmaku.java
@@ -1,6 +1,7 @@
 /**
  * Created by hrsonion on 16/5/24.
  */
+package danmaQ;
 import com.trolltech.qt.core.QPropertyAnimation;
 import com.trolltech.qt.core.QRect;
 import com.trolltech.qt.core.QTimer;
@@ -64,7 +65,7 @@ public class Danmaku extends QLabel{
         TOP,
         BOTTOM,
         FLY;
-    } // 枚举没有初始值 TOP = 1
+    }
     static final int VMARGIN;
 
     static {
@@ -79,10 +80,7 @@ public class Danmaku extends QLabel{
         String tcolor = colormapFirst(color); //
         QColor bcolor = colormapSecond(color); //
 
-        String style = style_tmpl
-                .arg(this.app.fontSize)
-                .arg(this.app.fontFamily)
-                .arg(tcolor);
+        String style = String.format(style_tmpl, this.app.fontSize, this.app.fontFamily, tcolor);
 
         QGraphicsDropShadowEffect effect = new QGraphicsDropShadowEffect(this);
 
@@ -125,7 +123,7 @@ public class Danmaku extends QLabel{
                 QRect(this._x, this._y, this.width(), this.height()));
 
         animation.setEndValue(
-                QRect(-this->width(), this->_y, this->width(), this->height()));
+                QRect(-this.width(), this._y, this.width(), this.height()));
         animation.start(QAbstractAnimation.DeleteWhenStopped);
 
         connect(
@@ -150,7 +148,7 @@ public class Danmaku extends QLabel{
     }
     // signals
 
-    private static String style_tmpl = new String("font-size: %1px;font-weight: bold;font-family: %2;color: %3;");
+    private static String style_tmpl = new String("font-size: %dpx;font-weight: bold;font-family: %s;color: %s;");
 
     private int _x, _y;
 
@@ -159,7 +157,7 @@ public class Danmaku extends QLabel{
     }
     private void init_position() {
         int sw = this.parentWidget().width();
-        this._y = this.dmwin.slot_y(this->slot);
+        this._y = this.dmwin.slot_y(this.slot);
 
         switch (this.position) {
             case FLY:
